03a5adfaf917a6de3a20cdbd9d7737a5dd05d93d
GreggMuffin <gregliguy@gmail.com>
2024-07-21 11:45:52 +1000

    - new nuget package GroupDocs.Metadata to edit the files metadata - new class MetadataEditor to handle metadata edit

---
+++ CowboyBipBoup/Model/MetadataEditor.cs
@@ -0,0 +1,40 @@
+﻿using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CowboyBipBoup.Model
+{
+    public static class MetadataEditor
+    {
+        public static void Wav(string fullPath, Log.Log? log = null)
+        {
+            //if file exists
+            if (File.Exists(fullPath))
+            {
+                //if file has the right extension
+                if(Path.GetExtension(fullPath) == ".wav")
+                {
+                    //edit its metadata
+
+                }
+                else
+                {
+                    log?.Error($"[MetadataEditor][Wav]\n" +
+                                $"File extension must be .wav:\n" +
+                                $"{fullPath}\n" +
+                                $"\n");
+                }
+            }
+            else
+            {
+                log?.Error($"[MetadataEditor][Wav]\n" +
+                            $"File does not exist:\n" +
+                            $"{fullPath}\n" +
+                            $"\n");
+            }
+        }
+    }
+}
